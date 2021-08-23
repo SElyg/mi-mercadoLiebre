76db59297cc2b752d0ab75a0e634750fa7ed6835
SElyg <elisa.gomezhdz@hotmail.com>
2021-08-22 22:36:22 -0500

    segundo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-let port= process.env.PORT || 3000;
+let port= process.env.port || 3000;
 app.listen(port, ()=>{
     console.log('Servidor funcionando');
 });
